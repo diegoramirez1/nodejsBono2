8b81f8beb818a9a1ed76828f07c0166248a31f35
Diego Ramirez <dramirez@mcclatchyservices.com>
2019-05-26 18:04:46 -0500

    agregando funciones.js

--- README.md
+++ README.md
@@ -1,11 +1,18 @@
 # nodejsBono2
 
+#si es con un solo archivo(asi lo pidieron)
+node funciones promedio -m=5 -l=1 -p=1
+
+#para fallos omita un parametro o el comando
+
+
+# si se podia hacer con requiere
 # para pruebas exitosa
-node principal promedio -m=3 -l=5 -p=5 -n=Diego
+node principal promedio -m=3 -l=5 -p=5
 en el browser abrir http://localhost:3000/
 
 #para pruebas fallidas
 #omitir algun parametro
-node principal promedio -m=3 -l=5 -p=5
+node principal promedio -m=3 -l=5
 #omitir el comando
 node principal -m=3 -l=5 -p=5 -n=Diego
--- funciones.js
+++ funciones.js
@@ -1,9 +1,33 @@
 const express = require('express')
 const app = express()
-const estudiantes = require ('./commandEstudiante');
+
+let opciones = {
+	matematicas:{
+		alias:'m',
+		demand : true
+	},
+	programacion:{
+		alias:'p',
+		demand : true
+	},
+	logica:{
+		alias:'l',
+		demand : true
+	}
+};
+
+
+let calcularPromedio = (notaMatematica,notaProgramacion,notaLogica) => {
+    return (notaMatematica+notaProgramacion+notaLogica)/3;
+}
+
+let mostrarMayores3 = (promedio) => {
+    mensaje = promedio > 3 ? 'El promedio es ' + promedio : "";
+    return mensaje;
+}
 
 const argv = require ('yargs')
-				.command('promedio','Calcular el pronedio ',estudiantes.opciones)
+				.command('promedio','Calcular el pronedio ',opciones)
 				.argv;
 
 
@@ -13,7 +37,7 @@ else {
 
     if(argv._ == "promedio"){
         
-        let mens = estudiantes.mostrarMayores3(estudiantes.calcularPromedio(argv.m,argv.p,argv.l,argv.n));         
+        let mens = mostrarMayores3(calcularPromedio(argv.m,argv.p,argv.l));         
 
         app.get('/', function (req, res) {
         res.send(mens);
@@ -27,4 +51,3 @@ else {
         console.log('Introdujo un comando invalido revisar el readme para mas informacion');
     }
 }
-
